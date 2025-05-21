ec1ead47639d3e0d7f06f9b14c54214b5991d5ac
Viacheslav-ua <atph@ukr.net>
2025-05-22 02:55:09 +0300

    created server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("API?");
+use std::env;
+
+use axum::Router;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+use tokio::net::TcpListener;
+
+#[derive(Serialize, FromRow)]
+struct Item {
+    id: i32,
+    name: String,
+    description: String,
+}
+
+#[derive(Deserialize)]
+struct RequestItem {
+    name: String,
+    description: String,
+}
+
+#[derive(Clone)]
+struct AppState {
+    db_pool: PgPool,
+}
+
+impl AppState {
+
+}
+
+async fn root() -> &'static str {
+    "Items API :)"
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_pool = PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to the database");
+
+    
+    let app_state = AppState { db_pool };
+    let app = Router::new()
+        .route("/", axum::routing::get(root)
+        .with_state(app_state));
+            
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
